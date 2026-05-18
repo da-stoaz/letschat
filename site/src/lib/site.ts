9e9fd63c4a56fdffbefa3159d7505a59137f9474
da-stoaz <felix.steinberger@proton.me>
2026-05-18 22:28:08 +0200

    feat: add roadmap page with milestones and priorities
    update design & layout

---
+++ site/src/lib/site.ts
@@ -0,0 +1,10 @@
+export const GITHUB_OWNER = "da-stoaz";
+export const GITHUB_REPO_NAME = "letschat";
+export const GITHUB_REPO = `${GITHUB_OWNER}/${GITHUB_REPO_NAME}`;
+
+export const GITHUB_BASE = `https://github.com/${GITHUB_REPO}`;
+export const GITHUB_ISSUES = `${GITHUB_BASE}/issues`;
+export const GITHUB_ISSUE_NEW = `${GITHUB_BASE}/issues/new/choose`;
+export const GITHUB_DISCUSSIONS = `${GITHUB_BASE}/discussions`;
+export const GITHUB_RELEASES = `${GITHUB_BASE}/releases`;
+export const LICENSE_NAME = "GNU GPLv3";
